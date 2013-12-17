5531c6bde5dfe9d2e3dddef8de991fabcfc12a06
Lowe Thiderman <lowe.thiderman@gmail.com>
2013-12-17 19:32:18 +0100

    The adventure begins

---
+++ doge.zsh
@@ -0,0 +1,14 @@
+#!/bin/zsh
+
+# This is a shellscript based presentation. Source this file to start the
+# presentation, then run the functions inside as we go along.
+# This talk was held at pysthlm meetup #16. http://www.pythonista.se
+# Written by Lowe Thiderman (lowe.thiderman@gmail.com)
+# Licensed under the MIT license
+
+# Hey guys
+
+# Tome has been bugging me for a while that I should hold a talk about this, so
+# I finally yielded and agreed to do it. I'll be doing this talk in
+# a non-standard format. Instead of slides, I'll just much around in my
+# terminals on my computer to show you some stuff!
